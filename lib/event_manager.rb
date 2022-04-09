7d00a79acdbb95a73d3c282fdbb3da3c5844f106
Manuel Gonçalves <mjgoncalves.trabalho@gmail.com>
2022-04-09 23:16:34 +0100

    Solution to assignment 2

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,6 +1,7 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
 require 'erb'
+require 'date'
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5,"0")[0..4]
@@ -61,6 +62,9 @@ contents = CSV.open(
 template_letter = File.read('form_letter.erb')
 erb_template = ERB.new template_letter
 
+prior_twelve = 0
+after_twelve = 0
+
 contents.each do |row|
   id = row[0]
   name = row[:first_name]
@@ -72,7 +76,19 @@ contents.each do |row|
 
   #save_thank_you_letter(id,form_letter) to uncomment in a later phase
   # homephone_method(row) solution to first assignment
-  puts row
+  date = row[:regdate]
+  data = date.split(" ")
+  hour = data[1].to_i
 
+  if hour < 12
+    prior_twelve += 1
+  elsif hour > 12
+    after_twelve += 1
+  end
 end
 
+if prior_twelve > after_twelve
+  puts "A good hour to send invite is between 0 and 12"
+elsif after_twelve > prior_twelve
+  puts "A good hour to send invite is between 12 and 24"
+end
